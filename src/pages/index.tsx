9471b5bc38df2d18f4b3607f5de8edd16c43059e
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-09-22 12:57:51 -0300

    :wrench: chore: pulling the posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
-import { FiCalendar, FiUser } from 'react-icons/fi'
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
 import Header from '../components/Header';
-
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -27,82 +30,81 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ postsPagination }: HomeProps) {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
 
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Como ultilizar Hooks</strong>
-              <p>Pensando em sincronização em vez de ciclos de vidas</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  15 Mar 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Tiago Augusto
-                </li>
-              </ul>
-            </a>
-          </Link>
-
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Criando um app CRA do zero</strong>
-              <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  19 Abr 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Tiago Augusto
-                </li>
-              </ul>
-            </a>
-          </Link>
-          <button type="button">
-            Carregar mais posts
-          </button>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
+          <button type="button"> Carregar mais posts </button>
         </div>
       </main>
     </>
   );
 }
 
-//export const getStaticProps: GetStaticProps = async () => {
-//const prismic = getPrismicClient();
-//
-//  const postsResponse = await prismic.query([
-//    Prismic.predicates.at('document.type', 'posts')
-//  ], {
-//    fetch: ['posts.title', 'posts.content'],
-//    pageSize: 80,
-//  });
-//
-//  const posts = response.results.map(post => {
-//    return {
-//      slug: post.uid,
-//      title: RichText.asText(post.data.title),
-//      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
-//      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
-//        day: '2-digit',
-//        month: 'long',
-//        year: 'numeric'
-//
-//      })
-//    }
-//  })
-//
-//  return {
-//    props: {
-//      posts
-//    }
-//  }
-//};
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
