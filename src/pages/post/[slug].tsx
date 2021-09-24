fb04475d9aa7ec7adb081216ab309c53b48bb321
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-09-24 11:01:49 -0300

    :wrench: chore: finishing Header and styling Page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { Head } from 'next/document';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -27,35 +28,91 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }): PostProps {
-  <>
-    <Head>
-      <title>{post.title} | spacetraveling</title>
-    </Head>
+export default function Post({ post }: PostProps) {
+  console.log(post);
+  return (
+    <>
+      <Header />
+      <img src="/banner.png" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Rocketseat é a melhor!</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                25 mar 2021
+              </li>
+              <li>
+                <FiUser />
+                Tiago Augusto
+              </li>
+              <li>
+                <FiClock />
+                25 min
+              </li>
+            </ul>
+          </div>
 
-    <main>
-      <article>
-        <h1>{post.heading}</h1>
-        <time>{post.first_publication_date}</time>
-        <div
-          className={styles.postContent}
-          dangerouslySetInnerHTML={{ __html: post.content }}
-        />
-      </article>
-    </main>
-  </>
+          <article>
+            <h2>Título seção</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              <strong>Aut error praesentium et</strong>. Aut laudantium et ex
+              dignissimos culpa magni id, animi pariatur hic placeat excepturi
+              fugiat dolor sapiente praesentium{' '}
+              <a href="/">xpedita odit possimus iste vel</a> erepudiandae eaque
+              sint accusantium est! Dicta temporibus eaque consectetur, autem
+              suscipit inventore mollitia eligendi iure similique possimus
+              ipsam, sunt dolorum corrupti officia odit exercitationem quisquam
+              doloremque sed dolor voluptatem quasi? Odit debitis velit vitae
+              ratione sapiente, optio amet? Modi minus dolores eos cupiditate
+              laboriosam facere maiores.
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: true,
+  }
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
+  const response = await prismic.getByUID('posts', String(slug), {});
 
-//   // TODO
-// };
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
